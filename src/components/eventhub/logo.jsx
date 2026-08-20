5e8dace7e2b96ebc56875066747770635dbded49
ThatDudeKondd <ThatDudeKondd@protonmail.com>
2026-08-20 12:08:41 +0200

    Testing

--- src/components/eventhub/logo.jsx
+++ src/components/eventhub/logo.jsx
@@ -4,7 +4,7 @@ import logoImg from "./logo";
 export default function Logo({ className = "w-10 h-10" }) {
   return (
     <img
-      src={logoImg}
+      src={logoImg({ className: "Logo" })}
       alt="EventHub Logo"
       className={`object-cover rounded-2xl shadow-lg shadow-indigo-600/30 transition-transform duration-300 ${className}`}
     />
